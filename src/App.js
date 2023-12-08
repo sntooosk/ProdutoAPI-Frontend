c8c428f3dc4756a493a068692137c5ba80b9c18a
Juliano Santos <128374665+DevSntosx71@users.noreply.github.com>
2023-12-08 14:03:24 -0300

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,119 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Formulario from './Formulario';
+import Tabela from './Tabela';
 
 function App() {
+
+  const produto = {
+    codigo: 0,
+    nome: '',
+    marca: '',
+  }
+
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
+  const [objProduto, setObjProduto] = useState(produto)
+
+  useEffect(() => {
+    fetch("http://localhost:8080/listar")
+      .then(retorno => retorno.json())
+      .then(retorno_convertido => setProdutos(retorno_convertido))
+  }, []);
+
+  const aoDigitar = (e) => {
+    setObjProduto({ ...objProduto, [e.target.name]: e.target.value });
+
+  }
+  const limparFormulario = () => {
+    setObjProduto(produto);
+    setBtnCadastrar(true);
+  }
+  const selecionarProduto = (indice) => {
+    setObjProduto(produtos[indice]);
+    setBtnCadastrar(false);
+  }
+
+  const cadastrar = () => {
+    fetch("http://localhost:8080/cadastrar", {
+      method: 'post',
+      body: JSON.stringify(objProduto),
+      headers: {
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
+      }
+
+    })
+      .then(retorno => retorno.json())
+      .then(retorno_convertido => {
+
+        if (retorno_convertido.mensagem !== undefined) {
+          alert(retorno_convertido.mensagem)
+        } else {
+          setProdutos([...produtos, retorno_convertido])
+          alert('Produto cadastrado com sucesso')
+          limparFormulario();
+        }
+      })
+  }
+
+  const alterar = () => {
+    fetch("http://localhost:8080/alterar", {
+      method: 'put',
+      body: JSON.stringify(objProduto),
+      headers: {
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
+      }
+
+    })
+      .then(retorno => retorno.json())
+      .then(retorno_convertido => {
+
+        if (retorno_convertido.mensagem !== undefined) {
+          alert(retorno_convertido.mensagem)
+        } else {
+          alert('Produto Alterado com sucesso');
+          let vetorTemp = [...produtos];
+          let indice = vetorTemp.findIndex((p) => {
+            return p.codigo === objProduto.codigo;
+          });
+          vetorTemp[indice] = objProduto
+
+          setProdutos(vetorTemp);
+          limparFormulario();
+        }
+      })
+  }
+
+  const remover = () => {
+    fetch("http://localhost:8080/remover/" + objProduto.codigo, {
+      method: 'delete',
+      headers: {
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
+      }
+
+    })
+      .then(retorno => retorno.json())
+      .then(retorno_convertido => {
+        alert(retorno_convertido.mensagem);
+
+        let vetorTemp = [...produtos];
+        let indice = vetorTemp.findIndex((p) => {
+          return p.codigo === objProduto.codigo;
+        });
+        vetorTemp.splice(indice, 1)
+
+        setProdutos(vetorTemp);
+
+        limparFormulario();
+      })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar} cadastrar={cadastrar} obj={objProduto} cancelar={limparFormulario} remover={remover} alterar={alterar} />
+      <Tabela vetor={produtos} selecionar={selecionarProduto} />
     </div>
   );
 }
---
+++ src/Formulario.js
@@ -0,0 +1,24 @@
+function Formulario({ botao, eventoTeclado, cadastrar, obj, cancelar, remover, alterar }) {
+  return (
+    <form action="">
+      <input type="text" value={obj.nome} onChange={eventoTeclado} name="nome" placeholder="Nome:" className="form-control" />
+      <input type="text" value={obj.marca} onChange={eventoTeclado} name="marca" placeholder="Marca:" className="form-control" />
+
+      {
+        botao
+          ?
+          <input type="button" value="Cadastrar" onClick={cadastrar} className="btn btn-primary" />
+          :
+          <div>
+
+
+            <input type="button" value="alterar" onClick={alterar} className="btn btn-warning" />
+            <input type="button" value="remover" onClick={remover} className="btn btn-danger" />
+            <input type="button" value="Cancelar" onClick={cancelar} className="btn btn-secondary" />
+          </div>
+      }
+
+    </form>
+  )
+}
+export default Formulario;
